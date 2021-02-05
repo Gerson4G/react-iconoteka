2a265d765f5e50f0aab630ab6e4a74774b12df8a
Gerson Goatache <gersongoatache@gmail.com>
2021-02-04 19:52:17 -0500

    compiling to publish test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Icon} from './components/';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+        <Icon name="3DRotation"/>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
