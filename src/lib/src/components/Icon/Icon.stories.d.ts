890393a110941591913176966aaf25234a9ebea8
Gerson Goatache <gersongoatache@gmail.com>
2021-02-09 17:03:07 -0500

    ditribuite story to (to be able to attach within other projects)

---
+++ src/lib/src/components/Icon/Icon.stories.d.ts
@@ -0,0 +1,5 @@
+import { Story, Meta } from '@storybook/react';
+import { IconProps } from './types';
+declare const _default: Meta<import("@storybook/react").Args>;
+export default _default;
+export declare const Icon: Story<IconProps>;
---
+++ src/lib/src/components/Icon/Icon.stories.js
@@ -0,0 +1,31 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Icon = void 0;
+var jsx_runtime_1 = require("react/jsx-runtime");
+var index_1 = __importDefault(require("./index"));
+exports.default = {
+    title: 'Icon',
+    component: index_1.default,
+    argTypes: {
+        color: { control: 'color' },
+    },
+};
+var Icon = function (args) { return jsx_runtime_1.jsx(index_1.default, __assign({}, args), void 0); };
+exports.Icon = Icon;
+exports.Icon.args = {
+    name: 'AlarmSnooze',
+};
