823798cf1af840d5e258914f8d502f5666afdd8a
Gerson Goatache <gersongoatache@gmail.com>
2021-02-09 18:40:49 -0500

    improve storybook

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { withDesign } from 'storybook-addon-designs';
 import { Story, Meta } from '@storybook/react';
+
 import Component from './index';
 import { IconProps } from './types';
 
 export default {
   title: 'Icon',
   component: Component,
+  decorators: [withDesign],
   argTypes: {
     color: { control: 'color' },
   },
@@ -15,4 +18,9 @@ export const Icon: Story<IconProps> = (args) => <Component {...args} />;
 
 Icon.args = {
   name: 'AlarmSnooze',
+  className: '',
+  stroke: 'regular',
+  type: 'line',
+  height: 20,
+  width: 20,
 };
