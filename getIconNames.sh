d77cc452d63c0bfff5cb6b60a16f87f507f55ea4
Gerson Goatache <gersongoatache@gmail.com>
2021-04-18 16:15:16 -0500

    final fix

--- getIconNames.sh
+++ getIconNames.sh
@@ -1,6 +1,9 @@
 echo "export type IconName =" > src/components/Icon/IconName.ts &&
 cd src/components/Icon/Icons &&
-for name in * ; do cleanName="${name/Iconoteka/}"; echo "'${cleanName/.tsx/}' |" ; done >> ../IconName.ts &&
+names=()
+for name in * ; do step_1="${name/Iconoteka/}"; step_2="${step_1/Fill/}"; step_3="${step_2/Line/}"; cleanName="${step_3/.tsx/}"; names+=("$cleanName"); done
+uniqueNames=($(printf "%s\n" "${names[@]}" | sort -u))
+printf "'%s' |\n" "${uniqueNames[@]}" >> ../IconName.ts
 sed -i '$ d' ../IconName.ts &&
 truncate -s-3 ../IconName.ts &&
 echo ";" >> ../IconName.ts &&
