cd2857b3bf1c8fdfcb979ef8fe87713abe885095
Gerson Goatache <gersongoatache@gmail.com>
2021-04-18 15:51:55 -0500

    fix extension script

--- getIconNames.sh
+++ getIconNames.sh
@@ -1,6 +1,6 @@
 echo "export type IconName =" > src/components/Icon/IconName.ts &&
 cd src/components/Icon/Icons &&
-for name in * ; do echo "'${name/Iconoteka/}' |" ; done >> ../IconName.ts &&
+for name in * ; do cleanName="${name/Iconoteka/}"; echo "'${cleanName/.tsx/}' |" ; done >> ../IconName.ts &&
 sed -i '$ d' ../IconName.ts &&
 truncate -s-3 ../IconName.ts &&
 echo ";" >> ../IconName.ts &&
